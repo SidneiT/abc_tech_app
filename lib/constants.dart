ae0726ee990ba716bc4082c02dfc49f528f954d7
Sidnei Teixeira <sidnei.teixeira@outlook.com>
2023-05-21 00:00:32 -0300

    adiciona configuração para rodar http

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
-  static const url =
-      "http://abctech-env.eba-46ty2yn8.us-east-1.elasticbeanstalk.com";
+  // static const url = "http://abctech-env.eba-46ty2yn8.us-east-1.elasticbeanstalk.com";
+  static const url = "http://localhost";
+  // static const url = "http://10.0.2.2"; //Android
 }
